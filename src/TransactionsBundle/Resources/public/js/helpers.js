eef22e00c8b23e1f3705a3833052cd84fa773758
Antonio Martins <antoniom@backbase.com>
2018-02-27 00:07:37 +0000

    Changes to layout and some movement of js code

--- src/TransactionsBundle/Resources/public/js/helpers.js
+++ src/TransactionsBundle/Resources/public/js/helpers.js
@@ -7,4 +7,8 @@ $(document).ready(function() {
         $(this).next().toggle();
         console.log($(this).attr('data-year'));
     });
+
+    $("#select_all").click(function() {
+      $('input:checkbox').attr('checked','checked');
+    });
 });
