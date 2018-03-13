ba2624eb930107d7ab0f0dbba75e9733cbf5c7ed
ammartins <amanuel.martins@gmail.com>
2018-03-13 08:05:17 +0100

    WIP

--- src/ImporterBundle/Controller/DefaultController.php
+++ src/ImporterBundle/Controller/DefaultController.php
@@ -8,32 +8,46 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use ImporterBundle\Entity\Imported;
 use Symfony\Component\Finder\Finder;
 
+// use Symfony\Bundle\FrameworkBundle\Console\Application;
+// use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// use Symfony\Component\Console\Input\ArrayInput;
+// use Symfony\Component\Console\Output\BufferedOutput;
+// use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
-  /**
-   * @Route("/importer")
-   */
-  public function indexAction()
-  {
-    $finder = new Finder();
-    // Maybe I should make this a bit more secure :P
-    $finder->files()->in('../data')->name('*.TAB');;
+    /**
+     * @Route("/importer")
+     */
+    public function indexAction()
+    {
+        // $kernel = $this->get('kernel');
+        // $application = new Application($kernel);
+        // $application->setAutoExit(false);
+        // exit;
+        // $application = new Application($kernel);
+        // $application->setAutoExit(false);
 
-    foreach ($finder as $file) {
-      $path = explode("/", $file->getPath());
-      dump($path[count($path)-1]);
-      dump(count($path));
-      $import = new Imported();
-      $import->setFileName($file->getBasename());
-      $import->setImportedAt(date('d-M-Y'));
-      dump($import);
-      // dump(get_class_methods($import));
-      // dump(get_class_methods($file));
-      // dump($file->getPath());
-      // exit;
-      // dump($file->getRelativePathname());
+        $finder = new Finder();
+        // Maybe I should make this a bit more secure :P
+        $finder->files()->in('../data')->name('*.TAB');;
+
+        foreach ($finder as $file) {
+            $path = explode("/", $file->getPath());
+            $account = $path[count($path)-1];
+
+            dump($path[count($path)-1]);
+            $import = new Imported();
+            $import->setFileName($file->getBasename());
+            $import->setImportedAt(date('d-M-Y'));
+            dump($import);
+            // dump(get_class_methods($import));
+            // dump(get_class_methods($file));
+            // dump($file->getPath());
+            // exit;
+            // dump($file->getRelativePathname());
+        }
+        exit;
+        return $this->render('ImporterBundle:Default:index.html.twig');
     }
-    exit;
-    return $this->render('ImporterBundle:Default:index.html.twig');
-  }
 }
