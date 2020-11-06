55e85bf33b30074189af8e36add25365f94c60c5
António Martins <amanuel.martins@gmail.com>
2020-11-06 02:06:51 +0100

    Fixigin migrations

--- app/DoctrineMigrations/Version20170706105824.php
+++ app/DoctrineMigrations/Version20170706105824.php
@@ -15,7 +15,6 @@ class Version20170706105824 extends AbstractMigration
      */
     public function up(Schema $schema) : void
     {
-        $this->addSql("ALTER TABLE transaction_type ADD parent_id INT DEFAULT NULL;");
         $this->addSql("CREATE INDEX IDX_6E9D6988727ACA70 ON transaction_type (parent_id)");
     }
 
